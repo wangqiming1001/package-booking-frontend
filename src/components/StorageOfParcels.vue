9c2dd91fa494cb0cb7abbba28182fd3f28ff56bc
wangqiming1001 <512821390@qq.com>
2019-09-05 23:18:22 +0800

    增加<返回>按钮

--- src/components/StorageOfParcels.vue
+++ src/components/StorageOfParcels.vue
@@ -5,12 +5,13 @@
         <div id="storageForm">
              <span>运单号：</span><input v-model="formMessege.orderNumber" placeholder="请输入运单号"><br/>
              <span>收件人：</span><input v-model="formMessege.name" placeholder="请输入收件人姓名"><br/>
-             <span>电  话：</span><input v-model="formMessege.phone" placeholder="请输入收件人电话"><br/>
-             <span>重  量：</span><input v-model="formMessege.weight" placeholder="请输入订单重量"><br/><br/>
+             <span>电&ensp;&ensp;话：</span><input v-model="formMessege.phone" placeholder="请输入收件人电话"><br/>
+             <span>重&ensp;&ensp;量：</span><input v-model="formMessege.weight" placeholder="请输入订单重量"><br/><br/>
         </div>
     </form>
     <div id="">
             <button @click="makeSure">确定</button>
+            <button @click="callBack">返回</button>
     </div>
   </div>
 </template>
@@ -33,6 +34,10 @@ export default {
             makeSure: function () {
                 this.$store.dispatch('addPackage',this.formMessege);
                 //this.$router.push('/storageList');
+            },
+             callBack: function () {
+                this.$router.push('/storageList');
+
             }
         }
     }
